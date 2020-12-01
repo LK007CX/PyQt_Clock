c11c9eb4bb04fc224673ea1faa4290647d86ec86
RezoLee <38316640+LK007CX@users.noreply.github.com>
2020-12-01 12:03:56 +0800

    add

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from math import *
 import sys
-
+import datetime
 
 StyleSheet = '''
 #timeLabel {
     color: white;
-    font-size: 500px;
+    font-size: 300px;
+    
     font-family: MicroSoft Yahei;
+    
 }
-
 QWidget {
     background-color: red;
 }
-
 #exitPushButton {
-    max-height: 48px;
+    min-height: 50px;
+    min-width: 50px;
     border-top-right-radius: 0px; /*右上角圆角*/
     border-bottom-left-radius: 0px; /*左下角圆角*/
 }
@@ -27,11 +28,17 @@ QWidget {
 #exitPushButton:pressed {
     background-color: #ffe0b2;
 }
-
 #lifeLabel {
     color: white;
     font-size: 20px;
     font-family: MicroSoft Yahei;
+}
+#dateTimeLabel {
+    color: white;
+    font-size: 25px;
+    font-weight: normal;
+    font-family: MicroSoft Yahei;
+    font-style:italic;
 }
 '''
 
@@ -42,10 +49,10 @@ class ClockWidget(QWidget):
         self.setWindowTitle("PyQt时钟")
         # self.resize(400, 400)
         self.timeLabel = QLabel(self, objectName='timeLabel')
+        self.dateTimeLabel = QLabel(self, objectName='dateTimeLabel')
         self.lifeLabel = QLabel(self, objectName='lifeLabel')
         self.backend = BackendThread()
-        self.exitPushButton = QPushButton(QIcon('./image/exit.png'), '', objectName='exitPushButton')
-
+        self.exitPushButton = QPushButton(QIcon('./image/cc-exit.png'), '', objectName='exitPushButton')
         # Init QSystemTrayIcon
         self.tray_icon = QSystemTrayIcon(self)
 
@@ -54,19 +61,17 @@ class ClockWidget(QWidget):
     def init_ui(self):
         layout = QVBoxLayout()
         layout.setContentsMargins(50, 50, 50, 50)
-
         layout.addWidget(self.lifeLabel, 0, Qt.AlignRight)
         layout.addStretch()
         layout.addWidget(self.timeLabel, 0, Qt.AlignCenter)
+        layout.addWidget(self.dateTimeLabel, 0, Qt.AlignCenter)
         layout.addStretch()
         layout.addWidget(self.exitPushButton, 0, Qt.AlignRight)
 
         self.setLayout(layout)
 
         self.lifeLabel.setText("                永远相信\n美好的事情即将发生")
-        # self.lifeLabel.setWordWrap(True)
         self.lifeLabel.setAlignment(Qt.AlignRight)
-
 
         self.tray_icon.setIcon(self.style().standardIcon(QStyle.SP_ComputerIcon))
         show_action = QAction("Show", self)
@@ -81,35 +86,36 @@ class ClockWidget(QWidget):
         tray_menu.addAction(quit_action)
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
-
         self.exitPushButton.clicked.connect(self.exit_app)
+        self.exitPushButton.setIconSize(QSize(25, 25))
         self.backend.update_date.connect(self.handle_display)
         self.backend.start()
 
-    def handle_display(self, time):
+    def handle_display(self, time, dateTime):
         self.timeLabel.setText(time)
+        self.dateTimeLabel.setText(dateTime)
 
     def exit_app(self):
         self.hide()
         self.tray_icon.showMessage(
-            "Tray Program",
-            "Application was minimized to Tray",
+            "PyQt 桌面小时钟",
+            "程序被最小化到托盘",
             QSystemTrayIcon.Information,
             2000
         )
 
 
-
 class BackendThread(QThread):
 
-    update_date = pyqtSignal(str)
+    update_date = pyqtSignal(str, str)
 
     def run(self):
         while True:
-            data = QDateTime.currentDateTime()
-            current_time = data.toString("hh:mm")
-            self.update_date.emit(str(current_time))
-            self.sleep(60)
+            data = datetime.datetime.now()
+            current_time = data.strftime("%H:%M:%S")
+            current_dateTime = data.strftime("%A    %Y-%m-%d")
+            self.update_date.emit(str(current_time), str(current_dateTime))
+            self.sleep(1)
 
 
 if __name__ == "__main__":
